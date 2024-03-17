454c9fa89515cbfc8b2f1cd9d1684b39958ee1ff
Sorongos <dosora9@gmail.com>
2024-03-16 21:31:55 -0700

    음성 감지 감도, lte 비율 상수 분리

--- lib/constants/ZerothDefine.dart
+++ lib/constants/ZerothDefine.dart
@@ -1,10 +1,14 @@
 class ZerothDefine {
   // 웹소켓 연결 주소
-  // static const String MY_URL_test = 'wss://www.voiceai.co.kr:8889/client/ws/flutter';
-  static const String MY_URL_test = 'ws://192.168.1.100:8080';
+  static const String MY_URL_test = 'wss://www.voiceai.co.kr:8889/client/ws/flutter';
+  // static const String MY_URL_test = 'ws://192.168.1.100:8080';
 
-  static const int ZEROTH_RATE_44 = 44100;
+  static const int ZEROTH_RATE_44 = 44100; // 샘플링율
 
-  static const int ZEROTH_MONO = 1;
+  static const int ZEROTH_MONO = 1; // 채널수
+
+  static const double THRESHOLD = 0.7; // 기본 음성 감지 감도. 데시벨 단위로 계산하고 있기 때문에 값이 작을수록 감도가 높음
+
+  static const double LTE_RATIO = 0.98; // ste가 업데이트될 때 이전 lte를 현재 lte에 얼마나 반영할 것인지
 
 }
